ec04a7a9aeb7ea806d9678641da1fa687e98a6fa
Irsyad Danisaputra <irsyad.sp21@gmail.com>
2025-01-17 14:44:13 +0700

    edit: Loader

--- my-react-app/src/Components/Elements/Loader.jsx
+++ my-react-app/src/Components/Elements/Loader.jsx
@@ -1,11 +1,7 @@
 const Loader = () => {
   return (
     <section className="flex items-center justify-center h-screen">
-      <div className="flex space-x-4">
-        <span className="w-4 h-4 bg-primary-500 rounded-full animate-ping delay-100" />
-        <span className="w-4 h-4 bg-primary-500 rounded-full animate-ping delay-200" />
-        <span className="w-4 h-4 bg-primary-500 rounded-full animate-ping delay-300" />
-      </div>
+      <div className="w-16 h-16 border-4 border-t-primary-500 border-gray-200 rounded-full animate-spin"></div>
     </section>
   );
 };
